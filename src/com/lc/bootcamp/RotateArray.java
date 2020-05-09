7bdd01409f44fc2e6cb5e3f83f41cf79c24c31de
tianqi.xu <tianqi.xu@huifu.com>
2020-05-09 14:27:06 +0800

    辗转相除法，求最大公约数

--- src/com/lc/bootcamp/RotateArray.java
+++ src/com/lc/bootcamp/RotateArray.java
@@ -27,11 +27,54 @@ package com.lc.bootcamp;
  * 2020/5/9
  */
 public class RotateArray {
+
     public void rotate(int[] nums, int k) {
-        if (k % nums.length == 0) {
-            //
+        k = k % nums.length;
+        if (k == 0) {
+            // k是数组长度的整数倍，不需要移动
             return;
         }
 
+        // 最大公约数
+        int factor = maxNumber(k, nums.length);
+        for (int j = 0; j < factor; j++) {
+            singleRound(nums, k, j, nums.length / factor);
+        }
+
+    }
+
+    public void singleRound(int nums[], int k, int tempIndex, int len) {
+        int temp = nums[tempIndex];
+        for (int i = 1; i <= len; i++) {
+            if (i * k + tempIndex < nums.length) {
+                temp = temp + nums[i * k + tempIndex];
+                nums[i * k + tempIndex] = temp - nums[i * k + tempIndex]; // temp
+                temp = temp - nums[i * k + tempIndex]; // nums[i*k]
+            } else {
+                temp = temp + nums[i * k % nums.length + tempIndex];
+                nums[i * k % nums.length + tempIndex] = temp - nums[i * k % nums.length + tempIndex]; // temp
+                temp = temp - nums[i * k % nums.length + tempIndex]; // nums[i*k]
+            }
+        }
+    }
+
+    /**
+     * 辗转相除法
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    int maxNumber(int m, int n) {
+        int temp;
+        if (n > m) {
+            temp = n;
+            n = m;
+            m = temp;
+        }
+        if (m % n == 0) {
+            return n;
+        }
+        return maxNumber(n, m % n);
     }
 }
