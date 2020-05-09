66838297877edd22659879de13a51b7fead67b28
tianqi.xu <tianqi.xu@huifu.com>
2020-05-09 18:00:23 +0800

    update

---
+++ src/com/lc/bootcamp/PlusOne.java
@@ -0,0 +1,36 @@
+package com.lc.bootcamp;
+
+/**
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 示例 1:
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * <p>
+ * 示例 2:
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ *
+ * @author tianqi.xu
+ * 2020/5/9
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            digits[i] += 1;
+            if (digits[i] <= 9) {
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] result = new int[len + 1];
+        result[0] = 1;
+        return result;
+    }
+}
