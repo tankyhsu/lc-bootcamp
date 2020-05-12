3072d189b4198dcce51ae9888b03ff5fff973f61
tianqi.xu <tianqi.xu@huifu.com>
2020-05-12 22:27:15 +0800

    cheat

---
+++ src/com/lc/bootcamp/string/Needle.java
@@ -0,0 +1,82 @@
+package com.lc.bootcamp.string;
+
+/**
+ * 实现 strStr() 函数。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * <p>
+ * 示例 1:
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * <p>
+ * 示例 2:
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 说明:
+ * <p>
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ *
+ * @author tianqi.xu
+ * 2020/5/12
+ */
+public class Needle {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals("")) {
+            return 0;
+        }
+        return haystack.indexOf(needle);
+    }
+
+    /**
+     * Code shared by String and StringBuffer to do searches. The
+     * source is the character array being searched, and the target
+     * is the string being searched for.
+     *
+     * @param source       the characters being searched.
+     * @param sourceOffset offset of the source string.
+     * @param sourceCount  count of the source string.
+     * @param target       the characters being searched for.
+     * @param targetOffset offset of the target string.
+     * @param targetCount  count of the target string.
+     * @param fromIndex    the index to begin searching from.
+     */
+    static int indexOf(char[] source, int sourceOffset, int sourceCount,
+                       char[] target, int targetOffset, int targetCount,
+                       int fromIndex) {
+        if (fromIndex >= sourceCount) {
+            return (targetCount == 0 ? sourceCount : -1);
+        }
+        if (fromIndex < 0) {
+            fromIndex = 0;
+        }
+        if (targetCount == 0) {
+            return fromIndex;
+        }
+
+        char first = target[targetOffset];
+        int max = sourceOffset + (sourceCount - targetCount);
+
+        for (int i = sourceOffset + fromIndex; i <= max; i++) {
+            /* Look for first character. */
+            if (source[i] != first) {
+                while (++i <= max && source[i] != first) ;
+            }
+
+            /* Found first character, now look at the rest of v2 */
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + targetCount - 1;
+                for (int k = targetOffset + 1; j < end && source[j]
+                        == target[k]; j++, k++)
+                    ;
+
+                if (j == end) {
+                    /* Found whole string. */
+                    return i - sourceOffset;
+                }
+            }
+        }
+        return -1;
+    }
+}
