048b11b0451fccc28995e4162cf5efd404cb8c55
tianqi.xu <tianqi.xu@huifu.com>
2020-05-18 18:34:35 +0800

    链表开始

---
+++ src/com/lc/bootcamp/linkedList/RemoveNode.java
@@ -0,0 +1,57 @@
+package com.lc.bootcamp.linkedList;
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ * <p>
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ * <p>
+ * <p>
+ * 示例 1:
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * <p>
+ * 示例 2:
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ * <p>
+ * 说明:
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ *
+ * @author tianqi.xu
+ * 2020/5/18
+ */
+public class RemoveNode {
+
+    public void deleteNode(ListNode node) {
+        ListNode cursor = node;
+//        while(cursor.next!=null){
+//            if(cursor.val)
+//            cursor = cursor.next;
+//        }
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        public ListNode getNext() {
+            return next;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+    }
+}
