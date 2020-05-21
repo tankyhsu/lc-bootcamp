fba9c96b9ed32588de9906892eb821348e6b180f
tianqi.xu <tianqi.xu@huifu.com>
2020-05-21 17:07:26 +0800

    链表

---
+++ src/com/lc/bootcamp/linkedList/RemoveNthFromEnd.java
@@ -0,0 +1,67 @@
+package com.lc.bootcamp.linkedList;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ * 给定的 n 保证是有效的。
+ * <p>
+ * 进阶：
+ * 你能尝试使用一趟扫描实现吗？
+ */
+public class RemoveNthFromEnd {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode cursor = head.next;
+
+        if (cursor == null) {
+            return n >= 1 ? null : head;
+        }
+
+        int len = 2;
+        while (cursor.next != null) {
+            len++;
+            cursor = cursor.next;
+        }
+        len -= n;
+        if (len < 0) {
+            return head;
+        } else if (len == 0) {
+            return head.next;
+        }
+        cursor = head;
+        for (int i = 1; i < len; i++) {
+            cursor = cursor.next;
+        }
+
+        ListNode temp = cursor.next;
+        cursor.next = temp == null ? null : temp.next;
+//        temp.next = null;
+        return head;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        removeNthFromEnd(head, 2);
+    }
+}
---
+++ src/com/lc/bootcamp/linkedList/ReverseList.java
@@ -0,0 +1,91 @@
+package com.lc.bootcamp.linkedList;
+
+/**
+ * * 反转一个单链表。
+ * <p>
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ * @author tianqi.xu
+ * 2020/5/21
+ */
+public class ReverseList {
+
+    /**
+     * 迭代实现
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode right = head.next;
+        ListNode left = head;
+        left.next = null;
+
+
+        while (right.next != null) {
+            // 指向翻转
+            ListNode temp = right.next;
+            right.next = left;
+            left = right;
+            right = temp;
+        }
+        right.next = left;
+        return right;
+    }
+
+    /**
+     * 递归实现
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode right = head.next;
+        ListNode left = head;
+        left.next = null;
+        return reverse(left, right);
+    }
+
+    private static ListNode reverse(ListNode left, ListNode right) {
+        if (right.next == null) {
+            right.next = left;
+            return right;
+        } else {
+            // 指向翻转
+            ListNode temp = right.next;
+            right.next = left;
+            return reverse(right, temp);
+        }
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        reverseList2(head);
+    }
+}
